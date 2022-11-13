9dbd6fb749a16581ea047bb1c3ed044a5ac3380b
Cristhian Flores <bachacode@gmail.com>
2022-11-13 19:31:08 -0400

    cache

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,7 @@
+<?php return array (
+  'buttons.delete' => 'App\\Http\\Livewire\\Buttons\\Delete',
+  'buttons.featured' => 'App\\Http\\Livewire\\Buttons\\Featured',
+  'newsletter.form' => 'App\\Http\\Livewire\\Newsletter\\Form',
+  'post.index' => 'App\\Http\\Livewire\\Post\\Index',
+  'post.show-post' => 'App\\Http\\Livewire\\Post\\ShowPost',
+);
